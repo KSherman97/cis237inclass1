f39350df36b4a500f0ed90d02e3c32b4e02626cc
Kyle Sherman <ksherm337@gmail.com>
2016-09-13 17:35:46 -0400

    Added basic User Interface class

--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,13 +10,15 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+
             Employee myEmployee = new Employee(); // references the default constructor of the Employee class
-            myEmployee.FirstNameString = "Kyle";
-            myEmployee.LastNameString = "Sherman";
-            myEmployee.WeeklySalaryDecimal = 3000m;
+            
+            //myEmployee.FirstNameString = "Kyle";
+            //myEmployee.LastNameString = "Sherman";
+            //myEmployee.WeeklySalaryDecimal = 3000m;
 
             // Console.WriteLine("         Name: " + myEmployee.FirstNameString + " " + myEmployee.LastNameString + "\n" + "Weekly Salary: " + myEmployee.WeeklySalaryDecimal.ToString("C"));
-            Console.WriteLine("         Name: " + myEmployee + "\n" + "Weekly Salary: " + myEmployee.WeeklySalaryDecimal.ToString("C"));
+            //Console.WriteLine("         Name: " + myEmployee + "\n" + "Weekly Salary: " + myEmployee.WeeklySalaryDecimal.ToString("C"));
             // Console.WriteLine(myEmployee); // prints out the class name
 
             Employee[] employees = new Employee[10]; // create an array of 10 indexes
@@ -26,12 +28,35 @@ namespace cis237Inclass1
             employees[2] = new Employee("John", "Hampton", 5000m);  // index 3
             employees[3] = new Employee("RIP", "Harambe", 2500m);   // index 4
             employees[4] = new Employee("James", "Kirk", 1500.25m); // index 5
+            
+            
 
-            foreach (Employee employee in employees) // foreach(Employee(Type;like int) employee(pointer to Employee class) in employees(array))
+            UserInterface UI = new UserInterface();
+            int choice = UI.GetUserInput();
+
+            while (choice != 2)
             {
-                if(employee != null)
-                    Console.WriteLine("         Name: " + employee + "\n" + "YearlySalary: " + employee.YearlySalary().ToString("C"));
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    string allOutPut = "";
+                    foreach (Employee employee in employees) // foreach(Employee(Type;like int) employee(pointer to Employee class) in employees(array))
+                    {
+                        if (employee != null)
+                        {
+                            allOutPut += employee.ToString() + " " +
+                                employee.YearlySalary().ToString("c") + Environment.NewLine;
+                            //Console.WriteLine("         Name: " + employee + "\n" + "YearlySalary: " + employee.YearlySalary().ToString("C"));
+                        }
+                    }
+                    UI.PrintAllOutput(allOutPut);
+                }
+                Console.WriteLine("Press any Key to continue.");
+                Console.ReadKey();
+                Console.Clear();
+                choice = UI.GetUserInput();
             }
+
         }
     }
 }
---
+++ cis237Inclass1/UserInterface.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class UserInterface
+    {
+        // UserInterface is a service class
+        public int GetUserInput()
+        {
+            this.printMainMenu();
+
+            string input = Console.ReadLine();
+
+            while(input != "1" && input != "2")
+            {
+                Console.Clear();
+                Console.WriteLine("That is not a valid input!");
+                Console.WriteLine("Please try again.");
+                Console.WriteLine();
+
+                System.Threading.Thread.Sleep(2000);
+                Console.Clear();
+
+                this.printMainMenu();
+                input = Console.ReadLine();
+                
+            }
+            return Int32.Parse(input);
+        }
+
+        public void PrintAllOutput(string allOutput)
+        {
+            Console.WriteLine(allOutput);
+        }
+
+        private void printMainMenu()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Print List");
+            Console.WriteLine("2. Exit");
+        }
+
+    }
+}
