64ce2053c94c626e375909752e195e5a3cfcbdd1
Kyle Sherman <ksherm337@gmail.com>
2016-09-13 16:48:43 -0400

    Added an Employee class Print data stored in an array.

---
+++ cis237Inclass1/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Employee
+    {
+
+        // *************************************************
+        //             Backing fields(Variables)
+        // *************************************************
+        private string _firstNameString;
+        private string _lastNameString;
+        private decimal _weeklySalaryDecimal;
+
+        // *************************************************
+        //                    Properties
+        // *************************************************
+        public string FirstNameString
+        {
+            get { return _firstNameString; }
+            set { _firstNameString = value; }
+        }
+
+        public string LastNameString
+        {
+            get { return _lastNameString; }
+            set { _lastNameString = value; }
+        }
+
+        public decimal WeeklySalaryDecimal
+        {
+            get { return _weeklySalaryDecimal; }
+            set { _weeklySalaryDecimal = value; }
+        }
+
+        // *************************************************
+        //                 Public Methods
+        // *************************************************
+
+        // returns the Firstname and Lastname when printing myEmployee
+        public override string ToString()
+        {
+            return this._firstNameString + " " + this._lastNameString;
+        }
+
+        public decimal YearlySalary()
+        {
+            return this._weeklySalaryDecimal * 52;
+        }
+
+        // *************************************************
+        //                  Constructor
+        // *************************************************
+        public Employee(string firstNameString, string lastNameString, decimal weeklySalaryDecimal)
+        {
+            this._firstNameString = firstNameString;
+            this._lastNameString = lastNameString;
+            this._weeklySalaryDecimal = weeklySalaryDecimal;
+        }
+
+        // Defualt Constructor
+        public Employee() 
+        {
+            // do nothing
+        }
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,28 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee(); // references the default constructor of the Employee class
+            myEmployee.FirstNameString = "Kyle";
+            myEmployee.LastNameString = "Sherman";
+            myEmployee.WeeklySalaryDecimal = 3000m;
+
+            // Console.WriteLine("         Name: " + myEmployee.FirstNameString + " " + myEmployee.LastNameString + "\n" + "Weekly Salary: " + myEmployee.WeeklySalaryDecimal.ToString("C"));
+            Console.WriteLine("         Name: " + myEmployee + "\n" + "Weekly Salary: " + myEmployee.WeeklySalaryDecimal.ToString("C"));
+            // Console.WriteLine(myEmployee); // prints out the class name
+
+            Employee[] employees = new Employee[10]; // create an array of 10 indexes
+
+            employees[0] = new Employee("James", "Cameron", 1200m); // index 1
+            employees[1] = new Employee("Seba", "Weber", 5000m);    // index 2
+            employees[2] = new Employee("John", "Hampton", 5000m);  // index 3
+            employees[3] = new Employee("RIP", "Harambe", 2500m);   // index 4
+            employees[4] = new Employee("James", "Kirk", 1500.25m); // index 5
+
+            foreach (Employee employee in employees) // foreach(Employee(Type;like int) employee(pointer to Employee class) in employees(array))
+            {
+                if(employee != null)
+                    Console.WriteLine("         Name: " + employee + "\n" + "YearlySalary: " + employee.YearlySalary().ToString("C"));
+            }
         }
     }
 }
